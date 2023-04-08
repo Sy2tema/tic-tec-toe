20a835064091c19f88589859cc1790f6bb013d04
Sy2tem4 <bood00142@gmail.com>
2023-04-08 18:36:49 +0900

    :sparkles: 기능 추가
    Board.tsx:
    - 게임 보드를 렌더링하며 각 칸마다 상태값을 저장합니다.
    
    Square.tsx:
    - 테이블 내의 각 칸들을 표시합니다.
    
    코드 추가:
    - 각 칸이 비어있는 틱택토에서 칸을 클릭했을 때 'X'가 해당 칸에 표시되도록 수정했습니다.

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,35 +1,49 @@
 import React from "react";
 import Square from "./Square";
 
-class Board extends React.Component {
-    renderSquare(index: number) {
-      return <Square />;
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+interface BoardProps {}
+
+interface BoardState {
+  squares: Array<number | null>;
+}
+
+class Board extends React.Component<BoardProps, BoardState> {
+  constructor(props: BoardProps) {
+    super(props);
+    
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+  renderSquare(index: number) {
+    return <Square value={this.state.squares[index]} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Board;
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    }
+// 인터페이스를 통해 value prop의 타입을 선언
+interface SquareProps {
+  value: number | null;
+}
+
+interface SquareState {
+  value: string;
+}
+
+// Square 컴포넌트를 정의, prop과 state의 타입을 설정
+class Square extends React.Component<SquareProps, SquareState> {
+  // Square 컴포넌트의 생성자 메소드, props를 받아 super로 전달
+  constructor(props: SquareProps) {
+    super(props);
+
+    this.state = {
+      value: '',
+    };
+  }
+
+  render() {
+    const SquareState = () => {
+      this.setState({ value: "X" });
+    };
+
+    return (
+      <button className="square" onClick = {SquareState}>
+        {this.state.value || this.props.value}
+      </button>
+    );
+  }
 }
 
 export default Square;
